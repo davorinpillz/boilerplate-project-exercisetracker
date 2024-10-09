46dbd9c39895c912aa3d590a826f58cd8584103e
Igor Mrtvac <ddp1329@gmail.com>
2024-10-09 14:04:00 +0000

    dadf

--- index.js
+++ index.js
@@ -3,48 +3,20 @@ const app = express()
 const cors = require('cors')
 const { MongoClient } = require('mongodb');
 const mongoose = require('mongoose');
-
-
-mongoose.connect('mongodb+srv://ddp1329:clifton84@cluster0.x0i7l.mongodb.net/ex?retryWrites=true&w=majority&appName=Cluster0')
-
 require('dotenv').config()
-
-async function main() {
- const uri = "mongodb+srv://ddp1329:clifton84@cluster0.x0i7l.mongodb.net/ex?retryWrites=true&w=majority&appName=Cluster0"
- const client = new MongoClient(uri)
- try {
-  await client.connect()
-  await listDatabases(client)
- }
- catch (err) {
-  console.log(err)
- }
- finally {
-  await client.close();
-}
-}
-main().catch(console.error);
-
-async function listDatabases(client){
-  databasesList = await client.db().admin().listDatabases();
-  console.log("Databases:");
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
-
+mongoose.connect('mongodb+srv://ddp1329:clifton84@cluster0.x0i7l.mongodb.net/ex?retryWrites=true&w=majority&appName=Cluster0')
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
-  console.log("testsasdadgagagagsdgasdgasgasgdsdgsagasdgasets")
 });
-
-
-
-
 const userSchema = new mongoose.Schema({
   username: String,
-
+  exercises: [{
+    type: mongoose.Schema.ObjectId,
+    ref: "Exercise"
+  }]
 })
 const User = mongoose.model("User", userSchema)
 const exerciseSchema = new mongoose.Schema({
@@ -64,21 +36,16 @@ const logSchema = new mongoose.Schema({
   }]
 })
 const Log = mongoose.model("Log", logSchema)
-/*testsdgasgdsdfasd*/
 app.post('/api/users', function(req,res) {
   let newUser = new User({username: req.body.username})
    newUser.save()
   res.json(newUser)
-  
 })
-
 app.get('/api/users',  function(req,res) {
-  User.find().select("-__v").then((r) => {
+  User.find().select("-__v").select("-exercise").then((r) => {
     res.send(r)
   })
-  console.log("test")
 })
-
 app.post('/api/users/:_id/exercises', function(req,res) {
   if (req.body.date) {
   let date = new Date(req.body.date)
@@ -90,12 +57,9 @@ app.post('/api/users/:_id/exercises', function(req,res) {
   else {
     let newExercise = new Exercise({description: req.body.description, duration: req.body.duration, date: new Date().toUTCString()})
 newExercise.save()
+
   }
-
-
-
 })
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
