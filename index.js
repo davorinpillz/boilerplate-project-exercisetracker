bb60a83c245e08b0cacaf679d7e1841224e168bf
Igor Mrtvac <ddp1329@gmail.com>
2024-10-10 08:23:04 +0000

    get

--- index.js
+++ index.js
@@ -4,29 +4,35 @@ const cors = require('cors')
 const { MongoClient } = require('mongodb');
 const mongoose = require('mongoose');
 require('dotenv').config()
-mongoose.connect('mongodb+srv://ddp1329:clifton84@cluster0.x0i7l.mongodb.net/ex?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://ddp1329:clifton84@cluster0.x0i7l.mongodb.net/new?retryWrites=true&w=majority&appName=Cluster0')
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
-const userSchema = new mongoose.Schema({
+const Schema = mongoose.Schema
+const userSchema = new Schema({
   username: String,
   exercises: [{
-    type: mongoose.Schema.ObjectId,
+    type: Schema.Types.ObjectId,
     ref: "Exercise"
   }]
 })
 const User = mongoose.model("User", userSchema)
-const exerciseSchema = new mongoose.Schema({
+const exerciseSchema = new Schema({
   username: String,
   description: String,
   duration: Number,
-  date: String
+  date: String,
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }
 })
 const Exercise = mongoose.model("Exercise", exerciseSchema)
-const logSchema = new mongoose.Schema({
+const logSchema = new Schema({
   username: String,
   count: Number,
   log: [{
@@ -47,18 +53,34 @@ app.get('/api/users',  function(req,res) {
   })
 })
 app.post('/api/users/:_id/exercises', function(req,res) {
+  console.log(req.params._id)
   if (req.body.date) {
   let date = new Date(req.body.date)
     date = date.toUTCString()
-    let newExercise = new Exercise({description: req.body.description, duration: req.body.duration, date: date})
+    let newExercise = new Exercise({description: req.body.description, duration: req.body.duration, date: date, user: req.params._id})
+    console.log(newExercise)
     newExercise.save()
+    console.log(newExercise)
     res.json(newExercise)
   }
   else {
-    let newExercise = new Exercise({description: req.body.description, duration: req.body.duration, date: new Date().toUTCString()})
+    let newExercise = new Exercise({description: req.body.description, duration: req.body.duration, date: new Date().toUTCString(), user: req.params._id})
+    console.log(newExercise)
+
 newExercise.save()
+console.log(newExercise)
 
   }
+})
+app.get('/api/users/:_id/exercises', (req, res) => {
+  async function userFind() {
+  let foundUser = await User.find({_id: req.params._id}).populate('exercises').exec()
+  console.log(foundUser)
+
+    res.json(foundUser)
+}
+userFind()
+
 })
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
